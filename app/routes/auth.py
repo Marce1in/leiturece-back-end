19e7772b7392986fbd129447a78ed0105cd3126b
Marce1in <marcelo.desenvolvedor.contato@gmail.com>
2025-01-24 18:12:53 -0300

    Goodbye fedora

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -1,12 +1,25 @@
-from fastapi import APIRouter
+from typing import Annotated
+from fastapi import APIRouter, Body
+from pydantic import BaseModel, EmailStr, Field
+from app.database.db import db_dependency
 
 router = APIRouter(
     prefix="/auth",
     tags=["auth"],
 )
 
-@router.get("/authenticated")
-def authenticated():
+class UserRegister(BaseModel):
+    name: str = Field(min_length=1, max_length=64)
+    email: EmailStr = Field(max_length=255)
+    password: str = Field(min_length=4, max_length=1024)
+
+class UserLogin(BaseModel):
+    email: EmailStr = Field(max_length=255)
+    password: str = Field(min_length=4, max_length=1024)
+
+
+@router.get("/")
+def auth():
     pass
 
 @router.post("/login")
@@ -14,8 +27,9 @@ def login():
     pass
 
 @router.post("/register")
-def register():
-    pass
+def register(new_user: Annotated[UserRegister, Body()]):
+    return new_user
+
 
 @router.post("/logout")
 def logout():
