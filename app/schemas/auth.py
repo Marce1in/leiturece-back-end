5b1f5b624b6d294b225c5e9f52e9413c9f112966
Marce1in <marcelo.desenvolvedor.contato@gmail.com>
2025-02-17 04:34:50 -0300

    fix: Remove useless min_length 1 in UserRegister Model

--- app/schemas/auth.py
+++ app/schemas/auth.py
@@ -1,7 +1,7 @@
 from pydantic import BaseModel, EmailStr, Field
 
 class UserRegister(BaseModel):
-    name: str = Field(min_length=1, max_length=64)
+    name: str = Field(max_length=64)
     email: EmailStr = Field(max_length=255)
     password: str = Field(min_length=4, max_length=1024)
 
